f0176a49bbac66e789fe9a9d06a9a3a5754c7520
corin <corin@DESKTOP-PBPU8US>
2021-05-17 18:15:23 +0900

    qna 테이블

---
+++ ysnj/src/main/webapp/sql/qna.sql
@@ -0,0 +1,17 @@
+create table qna(
+  qna_num number not null primary key,
+  mem_qnum number,
+  mem_anum number,
+  qna_title varchar2(400),
+  qna_name varchar2(100) not null,
+  qna_cate number not null,
+  qna_question varchar2(4000),
+  qna_answer varchar2(4000),
+  qna_status number,
+  qna_qdate date not null,
+  qna_adate date not null,
+  constraint qna_fk1 foreign key (mem_qnum) references member (mem_num),
+  constraint qna_fk2 foreign key (mem_anum) references member (mem_num)
+);
+
+create sequence qna_seq;
