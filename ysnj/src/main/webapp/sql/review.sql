43a9783a8091832d2c3ad72c0a63fa76ee6fe953
YuJin <YuJin@DESKTOP-Q7U70TC>
2021-05-17 18:31:03 +0900

    리뷰 테이블 업로드

---
+++ ysnj/src/main/webapp/sql/review.sql
@@ -0,0 +1,29 @@
+/*리뷰 게시판*/
+create table review(
+	rev_num number not null,
+	mem_num number not null,
+	acc_num number not null,
+	rev_title varchar2(20) not null,
+	rev_content varchar2(2000),
+	rev_date date default sysdate,
+	rev_hits number,
+	constraint review_pk primary key (rev_num),
+	constraint review_fk1 foreign key (mem_num) references member (mem_num),
+	constraint review_fk2 foreign key (acc_num) references accommdation (acc_num)
+);
+
+/*리뷰 별점 관리*/
+create table review_star(
+	rst_num number not null,
+	acc_num number not null,
+	rst_count number,
+	mem_num number,
+	rst_date date default sysdate not null,
+	constraint review_star_pk primary key (rst_num),
+	constraint review_star_fk1 foreign key (acc_num) references accommdation (acc_num),
+	constraint review_star_fk2 foreign key (mem_num) references member (mem_num)
+);
+
+create sequence review_seq;
+
+	
